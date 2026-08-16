17065a946a0f24642d9348cefc44a838b6dfdb14
ramassociates <68013095+ramassociates@users.noreply.github.com>
2026-08-16 21:34:53 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/garudayaana/MainActivity.java
+++ app/src/main/java/com/example/garudayaana/MainActivity.java
@@ -1,1 +1,14 @@
+package com.example.garudayaana;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ഇവിടെ activity_main എന്ന ലേഔട്ട് ഫയൽ ആണ് ലോഡ് ചെയ്യുന്നത്
+        setContentView(R.layout.activity_main);
+    }
+}
 
